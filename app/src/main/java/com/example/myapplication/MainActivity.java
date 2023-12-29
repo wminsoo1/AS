7ab26f5c53825b20259a4a163d770eed4634c72f
wminsoo1 <wminsoo1@naver.com>
2023-12-29 11:20:44 +0900

    초기 설정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MaiasdnActivity extends AppCompatActivity {
-asd
-    @Overrideasdsadadasdasd
-    protected voiasdd onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedasdInstasdanceState);
-        setContentView(R.layout.asadctivity_main);
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstasdanceState);
+        setContentView(R.layout.activity_main);
     }
 }
