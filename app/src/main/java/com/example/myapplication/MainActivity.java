28e470140831e0059e17ed74d6b198c7c3a96e7f
wminsoo1 <wminsoo1@naver.com>
2023-12-29 11:17:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaiasdnActivity extends AppCompatActivity {
+asd
+    @Overrideasdsadadasdasd
+    protected voiasdd onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedasdInstasdanceState);
+        setContentView(R.layout.asadctivity_main);
+    }
+}
